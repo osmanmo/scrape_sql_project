33ad37b2d985eba7458c3fdb0034c77d3f186d10
osmanmo <osmanomohamed@gmail.com>
2023-02-22 21:46:12 -0600

    final stuff that will work

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup
 import requests
 import logging
 from selenium import webdriver
+from config import db_uri
+
+logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
@@ -52,27 +55,42 @@ def unzip_file(file_name):
             # return a list of files
             return zip_ref.namelist()
 
+
 def get_file_name_from_url(url):
     return url.split('/')[-1]
 
+def create_table(df, file_name, engine):
+    # remove the ending
+    table_name = file_name.split('.')[0]
+    table_name = table_name.replace('-', '_')
+    table_name = table_name.replace(' ', '_')
+    table_name = table_name.lower()
+    # take the first 60 character if the table name is longer than 60
+    if len(table_name) > 60:
+        table_name = table_name[:60]
+    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False, schema='public')
+
 
 def main():
+    engine = sqlalchemy.create_engine(db_uri)
     all_urls = get_list_of_urls_in_business_section(base_url)
     for url in all_urls:
         file_name = get_file_name_from_url(url)
         if file_name.endswith('csv'):
             df = pd.read_csv(url, encoding='latin-1')
             logger.info(f"was able to read csv file {file_name}")
+            create_table(df, file_name, engine)
 
         elif file_name.endswith('zip'):
             download_file(url, file_name)
             logger.info(f"I am trying to unzip {file_name} let's see if it works")
             extracted_files = unzip_file(file_name)
             if isinstance(extracted_files, list):
-                logger.info(f"unzipped {file_name} and found {len(extracted_files)} files")
                 for file in extracted_files:
+                    logger.info(f"trying to read {file}")
                     if file.endswith('csv'):
                         df = pd.read_csv(file, encoding='latin-1')
+                        create_table(df, file, engine)
                     elif file.endswith('xlsx'):
                         df = pd.read_excel(file)
 
@@ -83,6 +101,7 @@ def main():
             else:
                 if extracted_files.endswith('csv'):
                     df = pd.read_csv(extracted_files, encoding='latin-1')
+                    create_table(df, extracted_files, engine)
                 elif extracted_files.endswith('xlsx'):
                     df = pd.read_excel(extracted_files)
                 # print(df.head())
@@ -90,7 +109,7 @@ def main():
 
             os.remove(file_name)
 
-
+    engine.dispose()
 
 
 if __name__ == '__main__':
